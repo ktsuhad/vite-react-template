7355276b0fa9978e1261ec704a2ed6bc2ad976ef
Muhammed Rashid M P <alrashid4star@gmail.com>
2023-06-19 15:40:39 +0530

    initial commit ⚡️

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import { Hello } from './components/hello/Hello';
+
+function App() {
+  return (
+    <>
+      <Hello />
+    </>
+  );
+}
+
+export default App;
---
+++ src/app/components/hello/Hello.tsx
@@ -0,0 +1,7 @@
+export function Hello() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold">Hello World!</h1>
+    </>
+  );
+}
